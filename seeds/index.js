6d64d872e3bc80433967c70cfe35e4ed8dee589e
Casey Macaluso <macaluso.casey@gmail.com>
2022-06-20 20:57:27 -0500

    First large commit of Campfinder project, CRUD functionality

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/campfinder", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const randomNum = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
