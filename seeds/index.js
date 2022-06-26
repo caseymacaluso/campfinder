1c0bc12f8842c5d5e75d6687749c3fee1216f455
Casey Macaluso <macaluso.casey@gmail.com>
2022-06-25 20:25:50 -0500

    updated campground model to include new attributes

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const randomNum = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error est natus consequatur illo porro minima aut deleniti dolores nulla omnis. Excepturi, exercitationem porro facilis atque architecto maxime vel qui tenetur?",
+      price,
     });
     await camp.save();
   }
