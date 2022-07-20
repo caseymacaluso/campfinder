c36ade99c86a80a9d89ff378158ca0ec45e919d2
Casey Macaluso <macaluso.casey@gmail.com>
2022-07-20 17:18:59 -0500

    add seedAmount variable

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,8 @@ const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
+const seedAmount = 250;
+
 mongoose.connect("mongodb://localhost:27017/campfinder", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +20,7 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < seedAmount; i++) {
     const randomNum = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
