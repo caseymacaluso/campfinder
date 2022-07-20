8445853e9d1e98c1b94aae3fc94ad74134ea2d1e
Casey Macaluso <macaluso.casey@gmail.com>
2022-07-20 13:44:15 -0500

    default image, added map popup

--- public/js/showMap.js
+++ public/js/showMap.js
@@ -9,4 +9,11 @@ map.on("style.load", () => {
   map.setFog({}); // Set the default atmosphere style
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${campground.title}</h4><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
